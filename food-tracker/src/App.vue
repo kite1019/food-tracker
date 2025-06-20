8cff87fc8ab46075a4ff0755fa65ddb6006916af
kite1019 <kite.tw@gmail.com>
2025-06-20 15:40:59 +0800

    first commit

---
+++ food-tracker/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import FoodRecord from './components/FoodRecord.vue'
+import FoodCalendar from './components/FoodCalendar.vue'
+import FoodStats from './components/FoodStats.vue'
+</script>
+
+<template>
+  <div>
+    <h1>每日飲食追蹤器</h1>
+    <FoodRecord />
+    <FoodCalendar />
+    <FoodStats />
+  </div>
+</template>
+
+<style scoped>
+h1 { margin-bottom: 2em; }
+</style>
